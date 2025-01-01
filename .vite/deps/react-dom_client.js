0540481770f070487b66dfeff06e3fa199c45cf5
Fabrice Bokovi <Fabrice.Bokovi@virtualincision.com>
2025-01-01 15:50:07 -0800

    stuff

---
+++ .vite/deps/react-dom_client.js
@@ -0,0 +1,7 @@
+import {
+  require_client
+} from "./chunk-ADVQML2F.js";
+import "./chunk-UP6LQVYV.js";
+import "./chunk-TWJRYSII.js";
+import "./chunk-DC5AMYBS.js";
+export default require_client();
---
+++ src/Components/Pages/LandingPage.tsx
@@ -0,0 +1,48 @@
+import CircularText from "../CircularText";
+import ModelViewer from "../3d_camera";
+import IntroSection from "../TypewriterText";
+
+function LandingPage() {
+  return (
+    <>
+      <div className="h-full flex flex-col justify-between">
+
+        {/* "Title card" */}
+        <div className="flex flex-row h-full justify-between p-12 ">
+          <div className="flex items-center">
+            <IntroSection />
+          </div>
+
+          {/* Side elements (3d) */}
+          <div className="scroll-m-20 text-4xl font-extrabold tracking-tight w-1/2  flex justify-center items-center">
+            <ModelViewer 
+              // modelUrl="src/assets/models/camera.glb"
+              modelUrl="src/assets/models/camera.glb"
+              backgroundColor={'white'}
+              cameraPosition={[0, 0, 5]}
+              autoRotate={true}
+            />
+          </div>
+        </div>
+
+        {/* Bottom elements */}
+        <div>
+          <div className="flex flex-row justify-between px-12 items-center">
+            <div className="flex flex-col">
+              <h1 className="scroll-m-20 text-xl font-medium font-serif tracking-tight first:mt-0">
+                Currently Software Engineer at Virtual Incision.
+              </h1>
+              <h1 className="scroll-m-20 pb-4 text-xl font-medium font-serif tracking-tight first:mt-0">
+                Omaha, Ne
+              </h1>
+            </div>
+            <CircularText />
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default LandingPage;
